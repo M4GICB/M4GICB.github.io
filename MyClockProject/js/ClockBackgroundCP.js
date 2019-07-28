908a588106eae8968108a741012a8556fa3f61c7
M4GICB <53400594+M4GICB@users.noreply.github.com>
2019-07-28 11:47:05 -0400

    Upload clock files

---
+++ MyClockProject/js/ClockBackgroundCP.js
@@ -0,0 +1,100 @@
+function BGsetRed() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "#850101";
+  thePage.style.backgroundColor = "#850101";
+} // end setRed function
+
+function BGsetOrange() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "#cc6600";
+  thePage.style.backgroundColor = "#cc6600";
+} // end setRed function
+
+function BGsetYellow() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "#ca912a";
+  thePage.style.backgroundColor = "#ca912a";
+} // end setRed function
+
+function BGsetGreen() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "green";
+  thePage.style.backgroundColor = "green";
+} // end setRed function
+
+function BGsetBlue() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "blue";
+  thePage.style.backgroundColor = "blue";
+} // end setRed function
+
+function BGsetIndigo() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "indigo";
+  thePage.style.backgroundColor = "indigo";
+} // end setRed function
+
+function BGsetViolet() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "violet";
+  thePage.style.backgroundColor = "violet";
+} // end setRed function
+
+function BGsetPurple() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "purple";
+  thePage.style.backgroundColor = "purple";
+} // end setRed function
+
+function BGsetCyan() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "cyan";
+  thePage.style.backgroundColor = "cyan";
+} // end setRed function
+
+function BGsetPink() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "pink";
+  thePage.style.backgroundColor = "pink";
+} // end setRed function
+
+function BGsetBlack() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "black";
+  thePage.style.backgroundColor = "black";
+} // end setRed function
+
+function BGsetWhite() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "white";
+  thePage.style.backgroundColor = "white";
+} // end setRed function
+
+function BGsetGrey() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+  currentCP.style.backgroundColor = "grey";
+  thePage.style.backgroundColor = "grey";
+} // end setRed function
+
+function BGcustomColor() {
+  var thePage = document.getElementById('page');
+  var preview = document.getElementById('currentCP');
+
+  var zeColor = prompt("Enter your hex code: #");
+
+  currentCP.style.backgroundColor = "#" + zeColor;
+  thePage.style.backgroundColor = "#" + zeColor;
+}
---
+++ MyClockProject/js/MyClock2.js
@@ -0,0 +1,97 @@
+"use strict";
+
+function launch() {
+  var textElem = document.getElementById("clocktext");
+  var targetWidth = 0.9; // proportion offull screen targetWidth
+  var curFontSize = 20;
+  var milTime = false;
+
+  function checkFormat(isMilitaryTimeBoolean) {
+    if(!isMilitaryTimeBoolean) {milTime = true;}
+    else {milTime = false;}
+  } // end check format function
+
+  function getClockText(isMilitaryTimeBoolean) {
+    var d = new Date();
+
+    var theTime = "";
+    function twHR(time) {
+      time += ((d.getHours() + 11) % 12 + 1) + ":";
+      time += (10 > d.getMinutes() ? "0" : "") + d.getMinutes() + ":";
+      time += (10 > d.getSeconds() ? "0" : "") + d.getSeconds() + "\u00A0";
+      time += d.getHours() >= 12 ? "pm" : "am";
+      theTime = time;
+    } // end 12 hour time format function
+
+    function tfHR(time) {
+      time += d.getHours() + ":";
+      time += (10 > d.getMinutes() ? "0" : "") + d.getMinutes() + ":";
+      time += (10 > d.getSeconds() ? "0" : "") + d.getSeconds() + "\u00A0";
+      theTime = time;
+    } // end 24 hour time format function
+
+    if(isMilitaryTimeBoolean) {tfHR("");}
+    else if (!isMilitaryTimeBoolean) {twHR("");}
+    else {theTime = "ERROR..."; alert("Unexpected Error... Please Refresh the Page...");}
+    var finalTime = document.getElementById("time");
+    finalTime.innerHTML = theTime;
+
+    var clock = document.getElementById("clockText");
+
+    var dayOfWeek = "";
+    switch (d.getDay()) {
+      case 0: dayOfWeek = "Sunday"; break;
+      case 1: dayOfWeek = "Monday"; break;
+      case 2: dayOfWeek = "Tuesday"; break;
+      case 3: dayOfWeek = "Wednesday"; break;
+      case 4: dayOfWeek = "Thursday"; break;
+      case 5: dayOfWeek = "Friday"; break;
+      case 6: dayOfWeek = "Saturday";
+    } // end switch statement
+    document.getElementById("day").innerHTML = dayOfWeek;
+
+    var numOfMonth = d.getDate();
+
+    var month = "";
+    switch (d.getMonth()) {
+      case 0: month = "January"; break;
+      case 1: month = "February"; break;
+      case 2: month = "March"; break;
+      case 3: month = "April"; break;
+      case 4: month = "May"; break;
+      case 5: month = "June"; break;
+      case 6: month = "July"; break;
+      case 7: month = "August"; break;
+      case 8: month = "September"; break;
+      case 9: month = "October"; break;
+      case 10: month = "November"; break;
+      case 11: month = "Decemeber";
+    } // end month switch statement
+
+    var year = d.getFullYear();
+    document.getElementById("date").innerHTML = month + " " + numOfMonth + ", " + year;
+  } // end getClockText function
+
+  document.getElementById("switch").addEventListener("click", changeTime);
+  function changeTime() {
+    checkFormat(milTime);
+  } // end changeTime function
+
+
+  function updateClock() {
+    document.getElementById("currentTimeFormat").innerHTML = (milTime) ? "Current Time Format: 24 HR" : "Current Time Format: 12 HR";
+    getClockText(milTime);
+    setTimeout(updateClock, 300);
+  } // end updateClock function
+
+  function updateTextSize() {
+    for (var i = 0; 3 > i; i++) {
+      curFontSize = 64;
+      textElem.style.curFontSize = curFontSize + "pt";
+    } // end for loop
+  } // end updateTextSize function
+
+  updateClock();
+  updateTextSize();
+  window.addEventListener("resize", updateTextSize);
+}
