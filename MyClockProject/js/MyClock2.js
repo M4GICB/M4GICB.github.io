89cd8b93a8ed14dca4862b17853e71db09f36e7b
M4GICB <53400594+M4GICB@users.noreply.github.com>
2019-08-02 09:53:22 -0400

    Removal of time format button

--- MyClockProject/js/MyClock2.js
+++ MyClockProject/js/MyClock2.js
@@ -72,12 +72,14 @@ function launch() {
     document.getElementById("date").innerHTML = month + " " + numOfMonth + ", " + year;
   } // end getClockText function
 
-  document.getElementById("switch").addEventListener("click", changeTime);
-  function changeTime() {
-    checkFormat(milTime);
-  } // end changeTime function
+  /* This section was originally button functionality for changing the time format.
+   * It is commented out because the button was replaced with a slider. */
+  // document.getElementById("switch").addEventListener("click", changeTime);
+  // function changeTime() {
+  //   checkFormat(milTime);
+  // } // end changeTime function
 
-  // functionality of the slider within the settings menu
+  // functionality of the time format slider within the settings menu
   var timeSlider = document.getElementById('theTimeToggle');
   timeSlider.addEventListener('change', e => {changeTime();});
 
